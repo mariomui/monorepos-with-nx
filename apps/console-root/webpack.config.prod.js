58ad542e639fee5c182977d37aa91d66909d1d93
mariomui <yi.kan.mui@gmail.com>
2022-09-14 21:57:16 -0700

    Create a console host.
    Reset to base.
    
    Remove console from workspace.
    
    Create a host app.
    
    Create unworking.
    
    Create a build script to see actual code.
    
    Setup db and shell by hand.

---
+++ apps/console-root/webpack.config.prod.js
@@ -0,0 +1,21 @@
+const withModuleFederation = require('@nrwl/react/module-federation');
+const moduleFederationConfig = require('./module-federation.config');
+const { merge } = require('webpack-merge');
+
+const federatedWebpack = withModuleFederation({
+  ...moduleFederationConfig,
+  remotes: [],
+});
+
+const polyfillConfig = {
+  mode: 'production',
+  devtool: false,
+};
+
+module.exports = new Promise((resolve) =>
+  federatedWebpack.then((fn) => {
+    resolve((config) => fn(merge(config, polyfillConfig)));
+  })
+);
+
+// https://github.com/nrwl/nx/issues/10485#issuecomment-1204102023
