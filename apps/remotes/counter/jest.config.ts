53ce34628ddbb3d1cb68a0b01eab1902a572aea4
mariomui <yi.kan.mui@gmail.com>
2022-09-18 15:20:58 -0700

    Create a app for a counter remote.

---
+++ apps/remotes/counter/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'remotes-counter',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/apps/remotes/counter',
+};
